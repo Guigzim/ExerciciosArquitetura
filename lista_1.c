8068b129f6b9a3dacbbd0bf28f404f2b4bae6601
Guilherme de Barros da Rocha <guilhermebarrosr@hotmail.com>
2019-09-28 18:54:44 -0300

    Finalização da lista 01 (pratica)

--- lista_1.c
+++ lista_1.c
@@ -5,15 +5,110 @@
 
 int unidadeToNum(char unidade); //Converte unidade de letra para expoente Ex: G para 30
 char numToUnidade(int num); //Converte unidade de expoente para letra Ex 30 para G
-void exercicio1(); //Executa funções do exercício 01
-void exercicio02();
+void exercicio01();//Executa funções do exercício 01
+void exercicio02();//Executa funções do exercício 02
+void exercicio03();//Executa funções do exercício 03
+void exercicio04();//Executa funções do exercício 04
+
 
 int main()
 {
+    printf("\n##### EXERCICIO 01 #####\n");
+    exercicio01();
+    printf("\n##### EXERCICIO 02 #####\n");
     exercicio02();
-
+    printf("\n##### EXERCICIO 03 #####\n");
+    exercicio03();
+    printf("\n##### EXERCICIO 04 #####\n");
+    exercicio04();
     return 0;
+}
+void exercicio04(){
+    double tamanho_MP;
+    printf("Digite o tamanho da memoria principal em bytes e sem unidade: ");
+    scanf("%lf", &tamanho_MP);
 
+    printf("\nAspas vazias ('') para sem unidade\nK para Kilo\nM para Mega\nG para Giga\nT para Tera\n");
+    printf("Digite a unidade do tamanho acima: ");
+    char unidade_MP;
+    scanf("%s", &unidade_MP);
+    double total_MP = tamanho_MP * pow(2, unidadeToNum(unidade_MP));
+
+    double tamanho_MC;
+    printf("Digite o tamanho da memoria cache em bytes e sem unidade: ");
+    scanf("%lf", &tamanho_MC);
+
+    printf("\nAspas vazias ('') para sem unidade\nK para Kilo\nM para Mega\nG para Giga\nT para Tera\n");
+    printf("Digite a unidade do tamanho acima: ");
+    char unidade_cache;
+    scanf("%s", &unidade_cache);
+    double total_MC = tamanho_MC * pow(2, unidadeToNum(unidade_cache));
+
+    printf("Digite a largura da linha da memoria cache em bytes: ");
+    double largura_linha_cache;
+    scanf("%lf", &largura_linha_cache);
+
+    printf("Digite a quantidade de linhas por conjunto: ");
+    int linhas_conjunto;
+    scanf("%d",&linhas_conjunto);
+
+    double total_linhas_cache = total_MC / largura_linha_cache;
+    double total_conjuntos = total_linhas_cache / linhas_conjunto;
+    double total_blocos = total_MP / largura_linha_cache;
+    double tag = log2((total_blocos / total_conjuntos));
+    double total_tag = tag * total_linhas_cache;
+    double total_MC_b = total_MC * 8;
+    double total_bits_cache = total_MC_b + total_tag;
+    int unidade_total_cache = 0;
+    if(total_bits_cache > 1024){
+        while(total_bits_cache > 2048){
+            total_bits_cache = total_bits_cache / 1024;
+            unidade_total_cache += 10;
+        }
+    }
+    printf("Total de bits para implementacao da cache eh: %.0lf %cbits", total_bits_cache, numToUnidade(unidade_total_cache));
+}
+void exercicio03(){
+    double tamanho_MP;
+    printf("Digite o tamanho da memoria principal em bytes e sem unidade: ");
+    scanf("%lf", &tamanho_MP);
+
+    printf("\nAspas vazias ('') para sem unidade\nK para Kilo\nM para Mega\nG para Giga\nT para Tera\n");
+    printf("Digite a unidade do tamanho acima: ");
+    char unidade_MP;
+    scanf("%s", &unidade_MP);
+    double total_MP = tamanho_MP * pow(2, unidadeToNum(unidade_MP));
+
+    double tamanho_MC;
+    printf("Digite o tamanho da memoria cache em bytes e sem unidade: ");
+    scanf("%lf", &tamanho_MC);
+
+
+    printf("\nAspas vazias ('') para sem unidade\nK para Kilo\nM para Mega\nG para Giga\nT para Tera\n");
+    printf("Digite a unidade do tamanho acima: ");
+    char unidade_cache;
+    scanf("%s", &unidade_cache);
+   
+    double total_MC = tamanho_MC * pow(2, unidadeToNum(unidade_cache));
+
+    printf("Digite a largura da linha da memoria cache em bytes: ");
+    double largura_linha_cache;
+    scanf("%lf", &largura_linha_cache);
+
+    double palavra = log2(largura_linha_cache);
+    double total_linhas_cache = total_MC / largura_linha_cache;
+    double tag = log2(total_MP / largura_linha_cache);
+    double total_tag = tag * total_linhas_cache;
+    double total_MC_b = total_MC * 8;
+    double total_bits_cache = total_MC_b + total_tag;
+    int unidade_total_cache=0;
+    if(total_bits_cache > 1024){
+        while(total_bits_cache > 2048){
+            total_bits_cache = total_bits_cache / 1024;
+            unidade_total_cache += 10;
+        }
+    }
+    printf("Total de bits para implementacao da cache eh: %.0lf %cbits", total_bits_cache, numToUnidade(unidade_total_cache));
 }
 void exercicio02(){
     /*Elabore  um  programa  para  calcular  e  mostrar  o  formato  de  endereço  de  um  
@@ -49,8 +144,6 @@ void exercicio02(){
     printf("Endereco Linha: %.0lf bits\n", EnderecoLinha);
     printf("Endereco Tag: %.0lf bits\n", EnderecoTagb);
 }
-
-
 int unidadeToNum(char unidade){
     switch (unidade)
     {
@@ -123,8 +216,6 @@ char numToUnidade(int num){
         break;
     }
 }
-
-
 void exercicio01(){
     /*Exercício 01 - solicita a quantidade de celulas da
     memória com unidade e o tamanho da célula em bits e retorna
